ebcf997074fd17e38b4df81ec08c148746248606
Xie Han <63350856@qq.com>
2025-05-08 01:28:41 +0800

    Add LICENSE

--- tutorial/tutorial-10-user_defined_protocol/message.cc
+++ tutorial/tutorial-10-user_defined_protocol/message.cc
@@ -12,8 +12,6 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-
-  Author: Xie Han (xiehan@sogou-inc.com;63350856@qq.com)
 */
 
 #include <errno.h>
